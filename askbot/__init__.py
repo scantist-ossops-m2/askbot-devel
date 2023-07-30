fed3a36386b24c82ed5ee30e398c282d947d9744
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2023-07-29 21:00:13 -0400

    bumps django-livesettings3 to 1.6.2

--- askbot/__init__.py
+++ askbot/__init__.py
@@ -33,7 +33,7 @@ REQUIREMENTS = {
     'jsonfield': 'jsonfield>=2.0.0',
     'jwt': 'pyjwt<=1.7.1',
     'keyedcache': 'django-keyedcache3>=1.5.1',
-    'livesettings': 'django-livesettings3==1.6.1',
+    'livesettings': 'django-livesettings3==1.6.2',
     'markdown2': 'markdown2<=2.3.9',
     'markupsafe': 'markupsafe',
     'mock': 'mock==3.0.5',
